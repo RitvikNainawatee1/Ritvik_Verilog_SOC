79e5102ffe02cd93f9f1daef74fec274529a0342
Ritvik Nainawatee <ritviknainawatee3@gmail.com>
2026-05-31 13:12:31 +0530

    created pc

--- Week 2/Assignment/pc.v
+++ Week 2/Assignment/pc.v
@@ -1,16 +1,11 @@
-// Week 2 — Program Counter (6-bit)
-// Priority: rst > load > inc > hold
-// Run: iverilog -o sim ../testbenches/tb_pc.v pc.v && vvp sim
+module pc(input clk, rst, inc, load, input [5:0] load_val, output reg [5:0] pc_out);
 
-module pc(
-    input            clk, rst, inc, load,
-    input      [5:0] load_val,
-    output reg [5:0] pc_out
-);
+    initial pc_out = 6'b0;
+
     always @(posedge clk) begin
-        // YOUR CODE HERE
-        // if rst:  pc_out = 0
-        // if load: pc_out = load_val
-        // if inc:  pc_out = pc_out + 1
+        if(rst) pc_out <= 6'b0; //reset
+        else if(load) pc_out <= load_val; //load value from input
+        else if(inc) pc_out = pc_out + 6'b000001; //increment
     end
+
 endmodule

0853a8eea7cae06f1edbc398a14bf47ac81e6673
Ritvik Nainawatee <ritviknainawatee3@gmail.com>
2026-05-31 13:12:54 +0530

    created regfile

--- Week 2/Assignment/regfile.v
+++ Week 2/Assignment/regfile.v
@@ -1,24 +1,20 @@
-// Week 2 — Register File (4 x 8-bit)
-// regs[0]=A, regs[1]=B, regs[2]=C, regs[3]=D
-// Reads: asynchronous | Write: synchronous, gated by we
-// Run: iverilog -o sim ../testbenches/tb_regfile.v regfile.v && vvp sim
+module regfile(input clk, input we, input [1:0] raddr0, raddr1, waddr, input[7:0] wdata, output [7:0] rdata0, output [7:0] rdata1);
 
-module regfile(
-    input        clk, we,
-    input  [1:0] raddr0, raddr1, waddr,
-    input  [7:0] wdata,
-    output [7:0] rdata0, rdata1
-);
-    reg [7:0] regs [3:0]; //4 registers, each 8 bits wide
+    reg [7:0] A,B,C,D;
 
-    always @(posedge clk) begin
-        if (we)
-            // YOUR CODE HERE — write wdata to regs[waddr]
-            ;
+    initial {A,B,C,D} = 32'b0;
+
+    always @(posedge clk, posedge we) begin
+        if(we) begin
+            case(waddr) 
+                2'b00: A <= wdata;
+                2'b01: B <= wdata;
+                2'b10: C <= wdata;
+                2'b11: D <= wdata;
+            endcase
+        end
     end
 
-    // YOUR CODE HERE — assign rdata0 and rdata1 from regs\
-    // assign rdata0 = ...
-    // assign rdata1 = ...
-
+    assign rdata0 = (raddr0[0])? ((raddr0[1])? D : B) : ((raddr0[1])? C : A); 
+    assign rdata1 = (raddr1[0])? ((raddr1[1])? D : B) : ((raddr1[1])? C : A); 
 endmodule
